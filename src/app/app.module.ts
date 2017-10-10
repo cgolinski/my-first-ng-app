fe1b9766df571e8de24c32ec908457671ddb4db7
cgolinski <cgolinski@cgolinskis-MacBook-Pro.local>
2017-10-10 17:13:36 -0400

    set up routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+import { DashboardComponent } from './dashboard.component';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
+
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroDetailComponent
-  ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
+   ],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroesComponent,
+    HeroDetailComponent
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,7 +4,6 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 
 @Injectable()
-
 export class HeroService {
   getHeroes(): Promise<Hero[]> {
     return Promise.resolve(HEROES);
@@ -12,7 +11,12 @@ export class HeroService {
 
   getHeroesSlowly(): Promise<Hero[]> {
     return new Promise(resolve => {
-      setTimeout(() => resolve(this.getHeroes()), 2000)
+      setTimeout(() => resolve(this.getHeroes()), 2000);
     });
+  }
+
+  getHero(id: number): Promise<Hero> {
+    return this.getHeroes()
+      .then(heroes => heroes.find(hero => hero.id === id));
   }
 }
